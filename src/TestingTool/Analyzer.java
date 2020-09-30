d5d6c7ac3b1401f0c6d968322e75c676b96133dd
Adrian Estevez <estevezadrian95@gmail.com>
2020-09-29 23:32:35 -0300

    fix lineas comentadas

--- src/TestingTool/Analyzer.java
+++ src/TestingTool/Analyzer.java
@@ -5,42 +5,83 @@ public class Analyzer {
 		if (line.trim().length() < 2) {
 			return false;
 		}
-		
+
 		if (line.trim().substring(0, 2) == "//" || line.trim().contains("//")) {
 			return true;
 		}
-		
+
 		return false;
 	}
-	
+
 	public static int getTotalLines(String sourceCode) {
 		return sourceCode.split("\n").length;
 	}
-	
+
 	public static int getTotalCodeLines(String sourceCode) {
 		int count = 0;
 		String sourceSplitted[] = sourceCode.split("\n");
+		boolean openComment = false;
+
 		for (int i = 0; i < sourceSplitted.length; ++i) {
-			String line = sourceSplitted[i];
-			if (line.trim().length() != 0 && !isCommentLine(line)) {
+			String line = sourceSplitted[i].trim();
+
+			if (line.startsWith("/*") && !line.contains("*/")) {
+				openComment = true;
+			}
+
+			if (line.contains("*/")) {
+				openComment = false;
+			}
+
+			if (line.length() != 0 && !line.startsWith("//") && !openComment) {
 				count++;
 			}
+//			if (line.trim().length() != 0 && !isCommentLine(line)) {
+//				count++;
+//			}
 		}
 		return count;
 	}
-	
+
 	public static int getTotalCommentLines(String sourceCode) {
 		int count = 0;
 		String sourceSplitted[] = sourceCode.split("\n");
+
+		boolean openComment = false;
+
 		for (int i = 0; i < sourceSplitted.length; ++i) {
-			String line = sourceSplitted[i];
-			if (isCommentLine(line)) {
+
+			String line = sourceSplitted[i].trim();
+			if (openComment) {
 				count++;
+				if (line.matches("^.*\\*/.*$")) {
+					openComment = false;
+
+				}
+			} else {
+
+				if (line.startsWith("//")) {
+					count++;
+				} else if (line.matches("^[^/]*/\\*.*$")) {
+
+					if (!line.matches("^.*\\*/.*$")) {
+						openComment = true;
+					}
+					count++;
+
+				} else if (line.matches("^[^'\"]*//[^'\"]*$")) {
+					count++;
+				}
+
 			}
+
+/*			if (isCommentLine(line)) {
+				count++;
+			}*/
 		}
 		return count;
 	}
-	
+
 	public static int getCyclesComplexity(String sourceCode) {
 		return 0;
 	}
